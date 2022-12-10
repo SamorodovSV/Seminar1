5b19e666d62cf1ce6fad12c966108cf0c6d47e26
Samorodov Sergey <samorodovsv@yandex.ru>
2022-12-10 15:25:07 +0300

    Выполнил первое ДЗ

--- src/lesson1/HomeWork1.java
+++ src/lesson1/HomeWork1.java
@@ -1,7 +1,19 @@
 package lesson1;
 
 public class HomeWork1 {
-    public static void main(String[] args) {
+    private final static  int MIN_LENGTH = 5;
 
+    public static void main(String[] args) {
+        System.out.println(getSize(new int[] {1, 2}));
+        System.out.println(getSize(new int[] {1, 2, 3, 4, 5, 6}));
+
+    }
+
+    public static int getSize(int[] array) {
+        if (array.length < MIN_LENGTH) {
+            return -1;
+        }
+
+        return array.length;
     }
 }
